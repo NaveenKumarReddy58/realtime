3910aaac18af8fcf800a26caaeec314b65b3115c
Maninder <sl_maninder@os4w.de>
2023-03-04 14:25:26 +0530

    pkg up

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,43 +1,6 @@
-$(function () {
-  $(".SearchDiv").click(function () {
-    $(this).addClass("active");
-  });
-});
-
-
-$('.responsive').slick({
-  dots: true,
-  autoplay: true,
-  infinite: true,
-  speed: 900,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+jQuery(document).ready(function ($) {
+  setTimeout(function () {
+    $("#phone").intlTelInput();
+    console.log('setTimeout');
+  }, 2000);
 });
