2ae2bde574c79e83780d0c128eacc3b73b777905
Maninder <sl_maninder@os4w.de>
2023-03-04 15:00:18 +0530

    Input cp add

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,6 +1,12 @@
 jQuery(document).ready(function ($) {
+  $(".Pro_input_Field_Phone").intlTelInput({
+    separateDialCode: true,
+  });
+
   setTimeout(function () {
-    $("#phone").intlTelInput();
-    console.log('setTimeout');
+    $(".Pro_input_Field_Phone").intlTelInput({
+      separateDialCode: true,
+    });
+    console.log("intlTelInput");
   }, 2000);
 });
