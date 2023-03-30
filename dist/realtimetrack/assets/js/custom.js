402ae4fa9d68f54557f1aa276a4d3cf024c94c67
Maninder <sl_maninder@os4w.de>
2023-03-30 21:19:32 +0530

    dist ffiles added

---
+++ dist/realtimetrack/assets/js/custom.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function ($) {
+  $(".Pro_input_Field_Phone").intlTelInput({
+    separateDialCode: true,
+  });
+
+  setTimeout(function () {
+    $(".Pro_input_Field_Phone").intlTelInput({
+      separateDialCode: true,
+    });
+    console.log("intlTelInput");
+  }, 2000);
+
+  setInterval(() => {
+    $(".Pro_input_Field_Phone").intlTelInput({
+      separateDialCode: true,
+    });
+  }, 1000);
+
+  $(".side_menu").click(function () {
+    $(".main_box").toggleClass("main");
+  });
+});
